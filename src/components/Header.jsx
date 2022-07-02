e85e11a1fb203e904185f26242e15871a357b962
YamiRayts <yamilarayts@gmail.com>
2022-07-01 22:54:23 -0300

    Fix header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
           
           <div className="header carousel-item active" data-bs-interval="10000">
           
-            <img src="/img/los-secretos.webp" className="d-block w-100" alt="..."/>
-              <div className="carousel-caption d-block ">
+            <img src="/img/los-secretos.webp" className="d-block w-100" alt="dumbledore's secrets poster"/>
+              <div className="carousel-caption d-block">
                 <SearchArea/>
               </div>
           </div>
